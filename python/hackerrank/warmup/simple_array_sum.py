eb67e5ca81a7add6e68b88b3d96c9f482e7b2a8e
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-09-06 14:22:20 +0300

    Add second option

--- python/hackerrank/warmup/simple_array_sum.py
+++ python/hackerrank/warmup/simple_array_sum.py
@@ -1,7 +1,17 @@
 # Simple Array Sum
+# Passed only one test case
 def simple_array_sum(ar):
     arr = [int(value) for value in ar.split(' ')]
     result = sum(arr)
     return result
 
 print(simple_array_sum('1 2 3 4 10 11'))
+
+
+# Option 2
+# Passed all test cases
+n = input()
+arr = input()
+objects = [int(value) for value in arr.split(' ')]
+result = sum(objects)
+print(result)
