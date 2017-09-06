4fab016ea0487844616cb33a8084bd9ff40e3d9e
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-09-06 19:39:45 +0300

    Add Compare The Triplets

---
+++ python/hackerrank/warmup/compare_the_triplets.py
@@ -0,0 +1,14 @@
+# Compare the Triplets
+def solve(a0, a1, a2, b0, b1, b2):
+    alice = [a0, a1, a2]
+    bob = [b0, b1, b2]
+    alice_score = 0
+    bob_score = 0
+    for x in range(len(alice)):
+        if alice[x] > bob[x]:
+            alice_score += 1
+        if bob[x] > alice[x]:
+            bob_score += 1
+    return "{}{}".format(alice_score, bob_score)
+
+print(solve(5, 6, 7, 3, 6, 10))
