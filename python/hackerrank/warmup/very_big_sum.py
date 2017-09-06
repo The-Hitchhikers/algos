9a5d44ccc031f310194300c16fa0de3f9e2dd05f
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-09-06 19:54:25 +0300

    Add A Very Big Sum

---
+++ python/hackerrank/warmup/very_big_sum.py
@@ -0,0 +1,8 @@
+# A Very Big Sum
+def very_big_sum(n, ar):
+    total = 0
+    for x in range(n):
+        total += ar[x]
+    return total
+
+print(very_big_sum(5, [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]))
