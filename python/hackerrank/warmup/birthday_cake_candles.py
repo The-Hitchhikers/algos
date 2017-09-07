b156d8e80a664af5571c9a2081b62db8706d7dff
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-09-07 22:54:23 +0300

    Add Birthday Cake candles

---
+++ python/hackerrank/warmup/birthday_cake_candles.py
@@ -0,0 +1,10 @@
+# Birthday cake candles
+def birthday_cake_candles(n, ar):
+    m = max(ar)
+    total = 0
+    for x in range(n):
+        if ar[x] == m:
+            total += 1
+    return total
+
+print(birthday_cake_candles(10, [18, 90, 90, 13, 90, 75, 90, 8, 90, 43]))
