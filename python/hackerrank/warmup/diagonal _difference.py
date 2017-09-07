cbdc05a95f7a74f873572437428ef67c6a98694d
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-09-07 22:07:40 +0300

    Add second option for the diagonal difference

--- python/hackerrank/warmup/diagonal _difference.py
+++ python/hackerrank/warmup/diagonal _difference.py
@@ -1,4 +1,4 @@
-# # Diagonal Difference
+# Diagonal Difference
 def diagonal_difference(n, ar):
     first_diagonal = []
     second_diagonal = []
@@ -12,4 +12,13 @@ def diagonal_difference(n, ar):
     total = (first_result - second_result)
     return abs(total)
 
+
+# Second option
+def diagonal_diff(n, ar):
+    first_diagonal = sum([ar[x][x] for x in range(n)])
+    second_diagonal = sum([ar[x][n - x - 1] for x in range(n)])
+    result = abs(first_diagonal - second_diagonal)
+    return result
+
 print(diagonal_difference(3, [[11, 2, 4], [4, 5, 6], [10, 8, -12]]))
+print(diagonal_diff(3, [[11, 2, 4], [4, 5, 6], [10, 8, -12]]))
