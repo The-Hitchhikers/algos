5a0e6a03b5390fdaeb6a6f1a7505b9aae646bd75
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-09-10 13:05:13 +0300

    Add Grading students problem

--- python/hackerrank/implementation/grading_students.py
+++ python/hackerrank/implementation/grading_students.py
@@ -1,3 +1,14 @@
-def grading_students():
-    pass
+# Grading students
+def grading_students(n, ar):
+    result = []
+    for x in range(n):
+        grade = ar[x]
+        if grade >= 38:
+            if grade % 5 == 3:
+                grade += 2
+            elif grade % 5 == 4:
+                grade += 1
+        result.append(grade)
+    return result
 
+print(grading_students(4, [73, 67, 38, 33]))
