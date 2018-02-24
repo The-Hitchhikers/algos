2cd7d6c3abc04392515473a843b349a6bac72a8d
MichaelYusko <mykhaylo.yusko@gmail.com>
2018-02-24 23:13:46 +0200

    Add super_reduced_string challange

---
+++ python/hackerrank/strings/super_reduced_string.py
@@ -0,0 +1,11 @@
+def reduce_string(s):
+    letters = []
+    for i in range(len(s)):
+        if not letters or s[i] != letters[-1]:
+            letters += [s[i]]
+        else:
+            letters.pop()
+    if letters:
+        return ''.join(letters)
+    else:
+        return 'Empty String'
